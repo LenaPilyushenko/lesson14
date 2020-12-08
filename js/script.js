10885dc0bb667177ca608cc64e43b250fbab0b15
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-08 20:20:07 +0300

    feat(js/script.js):new feat)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
  
 const DomElement = function(selector = '.div', height = '100px', width = '200px', bg = 'red' , fontSize = '14px') {
     this.selector = selector;
-    console.log('selector: ', this.selector.slice(0, 1));
     this.height = height;
     this.width = width;
     this.bg = bg;
@@ -17,8 +16,10 @@ const DomElement = function(selector = '.div', height = '100px', width = '200px'
                                     width: ${width};
                                     background: ${bg};
                                     fontSize : ${fontSize} ;
+                                    textAlign: center;
+                                    
             `;
-            newDiv.textContent = 'Любой текст';
+            newDiv.textContent = 'Погнали';
         } 
         if (this.selector.slice(0, 1) === '#') {
             const newp= document.createElement('p');
@@ -29,13 +30,48 @@ const DomElement = function(selector = '.div', height = '100px', width = '200px'
                                 width: ${width};
                                 background: ${bg};
                                 fontSize : ${fontSize}  ;
+                                position: absolute;
+                              
             `;
-            newp.textContent = 'Любой текст';
+            newp.textContent = 'Погнали';
         }
     };
-    
+  
 };
 
-const myElem = new DomElement('#test', '150px', '300px', 'red', '14px');
+function moveMyElem(callback){
+  
+    const target = document.querySelector('.square');
+    const allStylee = window.getComputedStyle(target);
+     
+    const left = parseInt(allStylee.marginLeft);
+    const top = parseInt(allStylee.marginTop);
+     
+    switch(callback.keyCode){
+         
+        case 37:  // клавиша влево
+            if(left > 0)
+                target.style.marginLeft = left - 10 + "px";
+            break;
+        case 38:   // клавиша вверх
+            if(top > 0)
+                target.style.marginTop = top - 10 + "px";
+            break;
+        case 39:   // клавиша вправо
+            if(left < document.documentElement.clientWidth - 100)
+                target.style.marginLeft = left + 10 + "px";
+            break;
+        case 40:   // клавиша вниз
+            if(top < document.documentElement.clientHeight - 100)
+                target.style.marginTop = top + 10 + "px";
+            break;
+    }
+}
 
-myElem.elemCreate();
+const myElem = new DomElement('.square', '100px', '100px', 'pink', '14px');
+
+addEventListener('keydown', moveMyElem);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    myElem.elemCreate();
+});

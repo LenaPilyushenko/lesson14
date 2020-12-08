7f7f53fd66b6e18d27d70d0200337633f800685c
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-08 22:16:23 +0300

    feat(js/script.js):new feat

--- js/script.js
+++ js/script.js
@@ -7,36 +7,19 @@ const DomElement = function(selector = '.div', height = '100px', width = '200px'
     this.bg = bg;
     this.fontSize = fontSize;
     this.elemCreate = function() {
-        if (this.selector.slice(0, 1) === '.') {
-            const srt = this.selector.slice(1);
-            const newDiv = document.createElement('div');
-            newDiv.classList.add(srt);
-            document.body.append(newDiv);
-            newDiv.style.cssText = `height: ${height};
-                                    width: ${width};
-                                    background: ${bg};
-                                    fontSize : ${fontSize} ;
-                                    textAlign: center;
-                                    
-            `;
-            newDiv.textContent = 'Погнали';
-        } 
-        if (this.selector.slice(0, 1) === '#') {
-            const newp= document.createElement('p');
-            const srtId = this.selector.slice(1);
-            newp.id = srtId;
-            document.body.append(newp);
-            newp.style.cssText = `height: ${height};
-                                width: ${width};
-                                background: ${bg};
-                                fontSize : ${fontSize}  ;
-                                position: absolute;
-                              
-            `;
-            newp.textContent = 'Погнали';
-        }
+
+        let elem = (this.selector.slice(0,1) === '.') ? 'div' : (this.selector.slice(0,1) === '#') ? 'p' : null;
+        const srt = this.selector.slice(1);
+        const newDiv = document.createElement(elem);
+        document.body.append(newDiv);
+        newDiv.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; fontSize : ${fontSize} ; position:absolute; `;
+        newDiv.textContent = 'Прокатимся?=)';
+
+        (this.selector.slice(0,1) === '.') ?
+        newDiv.classList.add(srt) : (this.selector.slice(0,1) === '#') ? newDiv.id = srt : null;
+    
     };
-  
+    
 };
 
 function moveMyElem(callback){
@@ -44,26 +27,27 @@ function moveMyElem(callback){
     const target = document.querySelector('.square');
     const allStylee = window.getComputedStyle(target);
      
-    const left = parseInt(allStylee.marginLeft);
-    const top = parseInt(allStylee.marginTop);
+    const toleft = parseInt(allStylee.left);
+    const totop = parseInt(allStylee.top);
      
     switch(callback.keyCode){
          
         case 37:  // клавиша влево
-            if(left > 0)
-                target.style.marginLeft = left - 10 + "px";
+            if(toleft > 0)
+                target.style.left = toleft - 10 + "px";
+                console.log('target.style.left: ', target.style.left);
             break;
         case 38:   // клавиша вверх
-            if(top > 0)
-                target.style.marginTop = top - 10 + "px";
+            if(totop > 0)
+                target.style.top = totop - 10 + "px";
             break;
         case 39:   // клавиша вправо
-            if(left < document.documentElement.clientWidth - 100)
-                target.style.marginLeft = left + 10 + "px";
+            if(toleft < document.documentElement.clientWidth - 100)
+                target.style.left = toleft + 10 + "px";
             break;
         case 40:   // клавиша вниз
-            if(top < document.documentElement.clientHeight - 100)
-                target.style.marginTop = top + 10 + "px";
+            if(totop < document.documentElement.clientHeight - 100)
+                target.style.top = totop + 10 + "px";
             break;
     }
 }

245b22f5ede9dab0de37a3fe47675af90d7ab868
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-08 21:50:36 +0300

    fix(js/script.js):fix

--- js/script.js
+++ js/script.js
@@ -2,40 +2,26 @@
  
 const DomElement = function(selector = '.div', height = '100px', width = '200px', bg = 'red' , fontSize = '14px') {
     this.selector = selector;
-    console.log('selector: ', this.selector.slice(0, 1));
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
     this.elemCreate = function() {
-        if (this.selector.slice(0, 1) === '.') {
-            const srt = this.selector.slice(1);
-            const newDiv = document.createElement('div');
-            newDiv.classList.add(srt);
-            document.body.append(newDiv);
-            newDiv.style.cssText = `height: ${height};
-                                    width: ${width};
-                                    background: ${bg};
-                                    fontSize : ${fontSize} ;
-            `;
-            newDiv.textContent = 'Любой текст';
-        } 
-        if (this.selector.slice(0, 1) === '#') {
-            const newp= document.createElement('p');
-            const srtId = this.selector.slice(1);
-            newp.id = srtId;
-            document.body.append(newp);
-            newp.style.cssText = `height: ${height};
-                                width: ${width};
-                                background: ${bg};
-                                fontSize : ${fontSize}  ;
-            `;
-            newp.textContent = 'Любой текст';
-        }
+
+        let elem = (this.selector.slice(0,1) === '.') ? 'div' : (this.selector.slice(0,1) === '#') ? 'p' : null;
+        const srt = this.selector.slice(1);
+        const newDiv = document.createElement(elem);
+        document.body.append(newDiv);
+        newDiv.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; fontSize : ${fontSize} ;`;
+        newDiv.textContent = 'Любой текст';
+
+        (this.selector.slice(0,1) === '.') ?
+        newDiv.classList.add(srt) : (this.selector.slice(0,1) === '#') ? newDiv.id = srt : null;
+    
     };
     
 };
 
-const myElem = new DomElement('#test', '150px', '300px', 'red', '14px');
+const myElem = new DomElement('.test', '150px', '300px', 'red', '14px');
 
 myElem.elemCreate();
